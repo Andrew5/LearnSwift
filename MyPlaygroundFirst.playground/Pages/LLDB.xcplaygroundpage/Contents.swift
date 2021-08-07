de78b3200070e280156f9e3e585e81eb64895516
Mr. Zhao <localhost3585@gmail.com>
2021-08-08 02:22:15 +0800

    lldb的常见使用

---
+++ MyPlaygroundFirst.playground/Pages/LLDB.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+//lldb常用指令
+
+//单步运行 把子函数当作整体一步执行(源码级别)
+//thread step-over、next、n
+
+//单步运行 遇到子函数会进入子函数(源码级别)
+//thread step-in、step、s
+
+//单步运行 把子函数当作整体一步执行（汇编级别）
+//thread step-inst-over、nexti、ni
+
+//单步运行 遇到子函数会进入子函数(汇编级别)
+//thread step-inst、stepi、si
+
+//直接执行完当前函数的所有代码，返回到上一个函数（遇到断点会卡住）
+//thread step-out、finish
+
+
+//: [Next](@next)
